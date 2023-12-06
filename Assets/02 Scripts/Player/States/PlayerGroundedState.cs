55aae58a76941ed82a5e35e09a3157c70ffb2539
m1zum0no <84029967+m1zum0no@users.noreply.github.com>
2023-12-06 16:42:51 +0300

    added `PlayerGroundedState` view to `StateUIViewer`

--- Assets/02 Scripts/Player/States/PlayerGroundedState.cs
+++ Assets/02 Scripts/Player/States/PlayerGroundedState.cs
@@ -11,7 +11,7 @@ public class PlayerGroundedState : PlayerState
     public override void Enter()
     {
         base.Enter();
-
+        player.currentStateInfoViewer.UpdateStateUI("PlayerGroundedState");
         player.TrackConsecutiveJumps(false);
     }
 
